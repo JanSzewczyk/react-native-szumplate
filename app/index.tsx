6638d58f44f1c6b9316a9166d4f3d09c1c5fcd19
Jan Szewczyk <jan.szewczyk1997@gmail.com>
2025-09-21 21:21:38 +0200

    feat: update layout and index components to improve screen structure and styling

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,12 @@
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 bg-white">
+      <SafeAreaView className="flex-1 items-center justify-center">
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+      </SafeAreaView>
     </View>
   );
 }
