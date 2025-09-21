f3097988ed5f833fdf95b699bf9061fdbc7cae64
Jan Szewczyk <jan.szewczyk1997@gmail.com>
2025-09-21 21:27:24 +0200

    refactor: simplify `Index` component layout by merging `View` into `SafeAreaView`

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,10 @@
-import { Text, View } from "react-native";
+import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View className="flex-1 bg-white">
-      <SafeAreaView className="flex-1 items-center justify-center">
-        <Text>Edit app/index.tsx to edit this screen.</Text>
-      </SafeAreaView>
-    </View>
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </SafeAreaView>
   );
 }
